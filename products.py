2d288a588213f0ec18dada73b3ffbb1d2b36d335
MH Wu <apple49755@gmail.com>
2022-04-07 00:08:25 +0100

    add functions

--- products.py
+++ products.py
@@ -1,45 +1,59 @@
 import os # operating system
 
-# read file
-products = [ ]
-if os.path.isfile('products.csv'):
-    print('file found')
-    with open('products.csv', 'r', encoding = 'utf-8') as f:
+def read_file(filename):
+    products = [ ]
+    with open(filename, 'r', encoding = 'utf-8') as f:
         for line in f:
             if '產品,價錢' in line or 'product,price' in line:
                 continue
-            #s = line.strip().split(',')
+                #s = line.strip().split(',')
             name, price = line.strip().split(',')
             products.append([name, price])
-    print(products)
-
-else:
-    print('no file found')
+    return products
 
 # let user to input
-while True:
-    name = input('what is the name of your produc: ')
-    if name == 'q':
-        break
-    price = input('what is the price of this produc: ')
-    price = float(price)
-    #p = []
-    #p.append(name)
-    #p.append(price)
-    #p = [name, price]
-    #products.append(p)
-    products.append([name, price])
-#print(products)
-#print(products[0][0])
+def user_input(products):
+    while True:
+        name = input('what is the name of your produc: ')
+        if name == 'q':
+            break
+        price = input('what is the price of this produc: ')
+        price = float(price)
+        #p = []
+        #p.append(name)
+        #p.append(price)
+        #p = [name, price]
+        #products.append(p)
+        products.append([name, price])
+    print(products)
+    return products
 
 # print the record of purchase
-for p_line in products:
-    print('The price of', p_line[0], 'is', p_line[1])
+def print_products(products):
+    for p_line in products:
+        print('The price of', p_line[0], 'is', p_line[1])
 
 # write file
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-    f.write('product,price\n')
-    f.write('產品,價錢\n')
-    for p in products:
-        print(p[0], p[1])
-        f.write(p[0] + ',' + str(p[1]) + '\n')   
+def write_file(filename, products):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('product,price\n')
+        f.write('產品,價錢\n')
+        for p in products:
+            print(p[0], p[1])
+            f.write(p[0] + ',' + str(p[1]) + '\n') 
+
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('file found')
+        products = read_file(filename)
+    else:
+        print('no file found')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+
+main()
